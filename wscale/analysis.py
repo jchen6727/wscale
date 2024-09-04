6c06ac2d1204c9257c19a392f8794b951cee4943
James Chen <jchen.6727@gmail.com>
2024-09-04 11:49:52 -0500

    weightnorm analysis

--- wscale/analysis.py
+++ wscale/analysis.py
@@ -1,13 +1,16 @@
 import pandas
 from scipy.interpolate import interp1d
 
+EPSPNORM = 0.5
 df = pandas.read_csv('grid_search.csv')[['config/sec', 'config/weight', 'epsp']]
 secs = df['config/sec'].unique()
 
 for sec in secs:
+# for each section calculate the weight where the epsp at soma == 0.5
     entries = df[df['config/sec'] == sec].sort_values(by='config/weight')
-    print(entries)
+    #print(entries)
     weights = entries['config/weight']
     epsps = entries['epsp']
     f = interp1d(epsps, weights, fill_value='extrapolate')
-    print([*zip(weights, epsps)])
+    #print([*zip(weights, epsps)])
+    wnorm = f(EPSPNORM) / EPSPNORM
